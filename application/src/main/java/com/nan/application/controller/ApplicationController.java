b8ed10b638cb4dc5c4938b119d170ae7aec79ff1
TracyNan <756047809@qq.com>
2025-10-19 10:10:50 +0800

    add gateway

--- application/src/main/java/com/nan/application/controller/ApplicationController.java
+++ application/src/main/java/com/nan/application/controller/ApplicationController.java
@@ -4,11 +4,13 @@ import com.nan.application.feign.UserServiceClient;
 import com.nan.user.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/app")
 public class ApplicationController {
     @Autowired
     UserServiceClient userServiceClient;
--- userService/src/main/java/com/nan/user/controller/UserServiceController.java
+++ userService/src/main/java/com/nan/user/controller/UserServiceController.java
@@ -6,23 +6,25 @@ import com.nan.user.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 public class UserServiceController {
     @Autowired
     UserServiceImpl userService;
     @Autowired
     UserDao userDao;
 
-    @GetMapping("getUsers")
+    @GetMapping("/getUsers")
     public List<UserEntity> getUserList() {
         return userService.getUserInfo();
     }
 
-    @GetMapping("getUserByName/{name}")
+    @GetMapping("/getUserByName/{name}")
     public UserEntity getUserByName(@PathVariable String name) {
         return userDao.getUserByName(name);
     }
